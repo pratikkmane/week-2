b02e19f8922ff23c99df7385b2b9b9ef41f4a947
hamzaalmani <142227908+hamzaalmani@users.noreply.github.com>
2026-02-01 12:31:37 -0500

    Part 1 completed
    Implement function to calculate combinations of pennies and nickels for a given amount.

--- apputil.py
+++ apputil.py
@@ -2,9 +2,22 @@ import numpy as np
 
 
 # update/add code below ...
-
+"""
+Return all possible ways to make n pairs using a combination of pennies and nickels.
+"""
 def ways(n):
-    return None
+    result = []
+
+    for nickels in range(n // 5 + 1):
+        pennies = n - (nickels * 5)
+        result.append((pennies, nickels))
+
+    return result
+
+print("ways 12: ", ways(12))
+print("ways 20: ", ways(20))
+print("ways 3: ", ways(3))
+print("ways 0: ", ways(0))
 
 def lowest_score(names, scores):
     return None
